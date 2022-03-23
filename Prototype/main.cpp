21b28f2e50f935b95683feec8d7e70f5fc4b2bde
yuga_hikage <k020g1204@g.neec.ac.jp>
2022-03-23 16:03:18 +0900

    マウスとカメラ合体完了、プロトタイプ用プロジェクト追加

--- Prototype/main.cpp
+++ Prototype/main.cpp
@@ -9,9 +9,14 @@
 #include "Model.h"
 #include"SpriteCommon.h"
 #include"Sprite.h"
+#include"DebugCamera.h"
 
+using namespace DirectX;
 using XMFLOAT2 = DirectX::XMFLOAT2;
 using XMFLOAT3 = DirectX::XMFLOAT3;
+using XMFLOAT4 = DirectX::XMFLOAT4;
+using XMVECTOR = DirectX::XMVECTOR;
+using XMMATRIX = DirectX::XMMATRIX;
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
@@ -36,7 +41,14 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	input = new Input();
 	input->Initialize(winApp);
 
-	Object3d::StaticInitialize(dxCommon->GetDev(), winApp->window_width, winApp->window_height);
+	Object3d::StaticInitialize(dxCommon->GetDev());
+	DebugCamera* camera = nullptr;
+	camera = new DebugCamera(WinApp::window_width, WinApp::window_height, input);
+	Object3d::SetCamera(camera);
+
+	// カメラ注視点をセット
+	camera->SetTarget({ 0, 1, 0 });
+	camera->SetDistance(3.0f);
 
 #pragma endregion DirectX初期化処理
 
@@ -47,6 +59,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 
 	spriteCommon->LoadTexture(0, L"Resources/texture.png");
 	spriteCommon->LoadTexture(1, L"Resources/house.png");
+	spriteCommon->LoadTexture(2, L"Resources/player.png");
 
 	std::vector<Sprite*> sprites;
 	/*Sprite* sprite = Sprite::Create(spriteCommon, 0);
@@ -57,13 +70,13 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	{
 		int texNum = rand() % 2;
 
-		Sprite* sprite = Sprite::Create(spriteCommon, texNum, { 0,0 }, false, false);
+		Sprite* sprite = Sprite::Create(spriteCommon, 2, { 0,0 }, false, false);
 
-		sprite->SetPosition({ (float)(rand() % 1280),(float)(rand() % 720),0 });
+		//sprite->SetPosition({ (float)(rand() % 1280),(float)(rand() % 720),0 });
 
 		//sprite->SetRotation((float)(rand() % 360));
 
-		sprite->SetSize({ (float)(rand() % 400), (float)(rand() % 100) });
+		//sprite->SetSize({ (float)(rand() % 400), (float)(rand() % 100) });
 
 		sprite->TransferVertexBuffer();
 
@@ -72,26 +85,36 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 
 	}
 
-
 	Model* modelPost = Model::LoadFromOBJ("posuto");
 	Model* modelChr = Model::LoadFromOBJ("chr_sword");
 
 	Object3d* objPost = Object3d::Create();
 	Object3d* objChr = Object3d::Create();
 
-
 	objPost->SetModel(modelPost);
-	objChr->SetModel(modelPost);
+	objChr->SetModel(modelChr);
 
-	objPost->SetPosition({ -10,0,-5 });
-	objChr->SetPosition({ +10,0,+5 });
+	objPost->SetPosition({ 0,0,50 });
+	objChr->SetPosition({ 0,-25,-75 });
 
-	objPost->Update();
-	objChr->Update();
+	camera->SetTarget({ 0,0,-100 });
+	camera->SetEye({ 0, 0, 0 });
+
+	/*objPost->Update();
+	objChr->Update();*/
 
 #pragma endregion 描画初期化処理
 
 	int counter = 0; // アニメーションの経過時間カウンター
+
+	bool dirty = false;
+	float angleX = 0;
+	float angleY = 0;
+	float scaleX = 1.0f / (float)WinApp::window_width;
+	float scaleY = 1.0f / (float)WinApp::window_height;
+	bool viewDirty = false;
+	float distance = 20.0f;
+	XMMATRIX matRot = DirectX::XMMatrixIdentity();
 
 	while (true)  // ゲームループ
 	{
@@ -113,7 +136,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 
 		scale *= 360.0f;
 		objPost->SetModel(modelPost);
-		objChr->SetModel(modelPost);
+		objChr->SetModel(modelChr);
 		if (input->TriggerKey(DIK_0)) // 数字の0キーが押されていたら
 		{
 			OutputDebugStringA("Hit 0\n");  // 出力ウィンドウに「Hit 0」と表示
@@ -141,39 +164,70 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 
 		}
 
-		XMFLOAT3 PostPosition = objPost->GetPosition();
+		//XMFLOAT3 ChrPos = objChr->GetPosition();
+
+		// マウスの入力を取得
 		Input::MouseMove mouseMove = input->GetMouseMove();
-		Input::StickMove stickMove = input->GetStickMove();
-		// マウスの左ボタンが押されていたら
-		if (input->PushMouseLeft()) { PostPosition.x -= 1.0f; }
-		// マウスの右ボタンが押されていたら
-		if (input->PushMouseRight()) { PostPosition.x += 1.0f; }
-		// ホイールを前に動かしたら
-		if (mouseMove.lZ > 0) { PostPosition.z += 2.0f; }
-		// ホイールを後ろに動かしたら
-		if (mouseMove.lZ < 0) { PostPosition.z -= 2.0f; }
+		float dy = mouseMove.lX * scaleY;
+		angleY = -dy * XM_PI;
 
-		// ゲームパッドのスティックテスト(最小0、最大65535、真ん中32767)
-		if (stickMove.lX < 27767) { PostPosition.x -= 2.0f; }
-		if (stickMove.lX > 37767) { PostPosition.x += 2.0f; }
-		if (stickMove.lY < 27767) { PostPosition.y += 2.0f; }
-		if (stickMove.lY > 37767) { PostPosition.y -= 2.0f; }
+		// ボタンが押されていたらカメラを並行移動させる
+		if (input->PushKey(DIK_D))
+		{
+			XMVECTOR move = { 1.0f, 0, 0, 0 };
+			move = XMVector3Transform(move, matRot);
+			camera->MoveVector(move);
+		}
+		if (input->PushKey(DIK_A))
+		{
+			XMVECTOR move = { -1.0f, 0, 0, 0 };
+			move = XMVector3Transform(move, matRot);
+			camera->MoveVector(move);
+		}
+		if (input->PushKey(DIK_W))
+		{
+			XMVECTOR move = { 0, 0, 1.0f, 0 };
+			move = XMVector3Transform(move, matRot);
+			camera->MoveVector(move);
+		}
+		if (input->PushKey(DIK_S))
+		{
+			XMVECTOR move = { 0, 0, -1.0f, 0 };
+			move = XMVector3Transform(move, matRot);
+			camera->MoveVector(move);
+		}
 
-		// 右トリガー押し込み
-		//if (stickMove.lZ == 128) { PostPosition.y -= 2.0f; }
-		// 右トリガー押してない
-		//if (stickMove.lZ == 32767) { PostPosition.y += 2.0f; }
+		dirty = true;
 
-		// 左トリガー押し込み
-		//if (stickMove.lRz > 0) { PostPosition.y -= 2.0f; }
-		// 左トリガー押してない
-		//if (stickMove.lZ == -127) { PostPosition.y += 2.0f; }
+		if (dirty || viewDirty) {
+			// 追加回転分の回転行列を生成
+			XMMATRIX matRotNew = XMMatrixIdentity();
+			matRotNew *= XMMatrixRotationX(-angleX);
+			matRotNew *= XMMatrixRotationY(-angleY);
+			// 累積の回転行列を合成
+			// ※回転行列を累積していくと、誤差でスケーリングがかかる危険がある為
+			// クォータニオンを使用する方が望ましい
+			matRot = matRotNew * matRot;
 
+			// 注視点から視点へのベクトルと、上方向ベクトル
+			XMVECTOR vTargetEye = { 0.0f, 0.0f, -distance, 1.0f };
+			XMVECTOR vUp = { 0.0f, 1.0f, 0.0f, 0.0f };
 
-		objPost->SetPosition(PostPosition);
+			// ベクトルを回転
+			vTargetEye = XMVector3Transform(vTargetEye, matRot);
+			vUp = XMVector3Transform(vUp, matRot);
+
+			// 注視点からずらした位置に視点座標を決定
+			const XMFLOAT3& target = camera->GetTarget();
+			camera->SetEye({ target.x + vTargetEye.m128_f32[0], target.y + vTargetEye.m128_f32[1], target.z + vTargetEye.m128_f32[2] });
+			camera->SetUp({ vUp.m128_f32[0], vUp.m128_f32[1], vUp.m128_f32[2] });
+		}
+
+		//objChr->SetPosition(ChrPos);
 
 		objPost->Update();
 		objChr->Update();
+		camera->Update();
 		for (auto& sprite : sprites)
 		{
 			sprite->Update();
@@ -194,7 +248,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		spriteCommon->PreDraw();
 		for (auto& sprite : sprites)
 		{
-			//sprite->Draw();
+			sprite->Draw();
 		}
 
 		// ４．描画コマンドここまで
