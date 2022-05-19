f29499797f10b03b6cd48cdceb414423dcfaeff7
yuga_hikage <k020g1204@g.neec.ac.jp>
2022-05-19 11:05:29 +0900

    カメラ実装成功

--- 仮β(改)/Base/GameBase/Camera/DebugCamera.cpp
+++ 仮β(改)/Base/GameBase/Camera/DebugCamera.cpp
@@ -7,18 +7,18 @@ DebugCamera::DebugCamera(int window_width, int window_height)
 	: Camera(window_width, window_height)
 {
 	//assert(input);
-
 	//this->input = input;
+
 	// 画面サイズに対する相対的なスケールに調整
-	scaleX = 1.0f / (float)window_width;
-	scaleY = 1.0f / (float)window_height;
+	//scaleX = 1.0f / (float)window_width;
+	//scaleY = 1.0f / (float)window_height;
 }
 
 void DebugCamera::Update()
 {
-	bool dirty = false;
+	/*bool dirty = false;
 	float angleX = 0;
-	float angleY = 0;
+	float angleY = 0;*/
 
 	//// マウスの入力を取得
 	//Input::MouseMove mouseMove = input->GetMouseMove();
@@ -54,29 +54,29 @@ void DebugCamera::Update()
 		dirty = true;
 	}*/
 
-	if (dirty || viewDirty) {
-		// 追加回転分の回転行列を生成
-		XMMATRIX matRotNew = XMMatrixIdentity();
-		matRotNew *= XMMatrixRotationX(-angleX);
-		matRotNew *= XMMatrixRotationY(-angleY);
-		// 累積の回転行列を合成
-		// ※回転行列を累積していくと、誤差でスケーリングがかかる危険がある為
-		// クォータニオンを使用する方が望ましい
-		matRot = matRotNew * matRot;
+	//if (dirty || viewDirty) {
+	//	// 追加回転分の回転行列を生成
+	//	XMMATRIX matRotNew = XMMatrixIdentity();
+	//	matRotNew *= XMMatrixRotationX(-angleX);
+	//	matRotNew *= XMMatrixRotationY(-angleY);
+	//	// 累積の回転行列を合成
+	//	// ※回転行列を累積していくと、誤差でスケーリングがかかる危険がある為
+	//	// クォータニオンを使用する方が望ましい
+	//	matRot = matRotNew * matRot;
 
-		// 注視点から視点へのベクトルと、上方向ベクトル
-		XMVECTOR vTargetEye = { 0.0f, 0.0f, -distance, 1.0f };
-		XMVECTOR vUp = { 0.0f, 1.0f, 0.0f, 0.0f };
+	//	// 注視点から視点へのベクトルと、上方向ベクトル
+	//	XMVECTOR vTargetEye = { 0.0f, 0.0f, -distance, 1.0f };
+	//	XMVECTOR vUp = { 0.0f, 1.0f, 0.0f, 0.0f };
 
-		// ベクトルを回転
-		vTargetEye = XMVector3Transform(vTargetEye, matRot);
-		vUp = XMVector3Transform(vUp, matRot);
+	//	// ベクトルを回転
+	//	vTargetEye = XMVector3Transform(vTargetEye, matRot);
+	//	vUp = XMVector3Transform(vUp, matRot);
 
-		// 注視点からずらした位置に視点座標を決定
-		const XMFLOAT3& target = GetTarget();
-		SetEye({ target.x + vTargetEye.m128_f32[0], target.y + vTargetEye.m128_f32[1], target.z + vTargetEye.m128_f32[2] });
-		SetUp({ vUp.m128_f32[0], vUp.m128_f32[1], vUp.m128_f32[2] });
-	}
+	//	// 注視点からずらした位置に視点座標を決定
+	//	const XMFLOAT3& target = GetTarget();
+	//	SetEye({ target.x + vTargetEye.m128_f32[0], target.y + vTargetEye.m128_f32[1], target.z + vTargetEye.m128_f32[2] });
+	//	SetUp({ vUp.m128_f32[0], vUp.m128_f32[1], vUp.m128_f32[2] });
+	//}
 
 	Camera::Update();
 }
